d3e84af292bce5ac2657417d476522fe9141ebf9
LeonidGarasko <96500493+LeonidGarasko@users.noreply.github.com>
2022-06-26 13:48:28 +0000

    Deploying to gh-pages from @ LeonidGarasko/goit-js-hw-10@72b2c1673fd08f98a794681959857907c00e42a0 🚀

---
+++ index.4097d8a0.js
@@ -0,0 +1,2 @@
+!function(){var n;(n="Ukraine",fetch("https://restcountries.com/v3.1/name/".concat(n,"?fields=name.official,capital,population,flags.svg,languages")).then((function(n){throw n.ok&&n.json(),new Error(n.statusText)}))).then((function(n){return console.log(n)}))}();
+//# sourceMappingURL=index.4097d8a0.js.map
